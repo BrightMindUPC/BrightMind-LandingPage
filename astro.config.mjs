ec0970bcf407e71b0de7a98934a8e863e9d7b716
SgtGarro <marcelogarro137@gmail.com>
2024-10-31 21:21:35 -0500

    build: add sharp image adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: netlify()
-});
+  adapter: netlify(),
+  image: {
+    service: sharpImageService(),
+  },
+})
